17f1cda91c720fb3a728cb746d34693b33ca56e8
BENIE <sylvestrebenie7829@gmail.com>
2023-06-29 16:42:48 +0000

    gerer la page d'acueil

--- src/location/views.py
+++ src/location/views.py
@@ -4,7 +4,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "location/home.html")
 
 def camion_list(request):
     camions = Camion.objects.all()
